d4876d3b5c5d0a0f5ce6caf043afe492e1eabb4b
Dmitry <dmitry2595@gmail.com>
2023-09-29 19:38:47 +0300

    feat: add tab block

--- www/index.js
+++ www/index.js
@@ -1,1 +1,20 @@
 console.log('index.js connected')
+
+// tab 
+function openTab (evt, tabName) {
+    let i, tabsContent, tabsLinks;
+    tabsContent = document.getElementsByClassName('tab-content');
+    for (let i = 0; i < tabsContent.length; i++) {
+        tabsContent[i].style.display = 'none'
+    }
+
+    tabsLinks = document.getElementsByClassName('tab-button');
+    for (let i = 0; i < tabsLinks.length; i++) {
+        tabsLinks[i].className = tabsLinks[i].className.replace(' active', '');
+    }
+
+    document.getElementById(tabName).style.display = 'block';
+    evt.currentTarget.className += ' active';
+}
+// open default tab
+document.getElementById('deafault-open-tab').click();
