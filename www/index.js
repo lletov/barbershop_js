325acad055c81c3261915ce7e034a57ea884b0fb
Dmitry <dmitry2595@gmail.com>
2023-09-29 21:02:42 +0300

    feat: add discount block

--- www/index.js
+++ www/index.js
@@ -2,13 +2,12 @@ console.log('index.js connected')
 
 // tab 
 function openTab (evt, tabName) {
-    let i, tabsContent, tabsLinks;
-    tabsContent = document.getElementsByClassName('tab-content');
+    let tabsContent = document.getElementsByClassName('tab-content');
     for (let i = 0; i < tabsContent.length; i++) {
         tabsContent[i].style.display = 'none'
     }
 
-    tabsLinks = document.getElementsByClassName('tab-button');
+    let tabsLinks = document.getElementsByClassName('tab-button');
     for (let i = 0; i < tabsLinks.length; i++) {
         tabsLinks[i].className = tabsLinks[i].className.replace(' active', '');
     }
@@ -18,3 +17,27 @@ function openTab (evt, tabName) {
 }
 // open default tab
 document.getElementById('deafault-open-tab').click();
+
+// discount
+function getDiscount() {
+    let possibleDiscounts = ['5%', '10%', '15%', '20%'];
+    let number = possibleDiscounts[Math.floor(Math.random() * possibleDiscounts.length)];
+
+    let discountContent = document.getElementById('discount-number')
+    let loader = document.getElementById('loader')
+    let getDiscountButton = document.getElementById('get-discount-button')
+    
+    getDiscountButton.onclick = null;
+    discountContent.style.display = 'none'
+    loader.style.display = 'block'
+    setTimeout(() => {
+
+        getDiscountButton.onclick = getDiscount;
+        getDiscountButton.textContent = 'ПОПРОБОВАТЬ ЕЩЕ'
+        discountContent.style.color = '#000';
+        discountContent.textContent = number;
+        discountContent.style.display = 'block'
+        loader.style.display = 'none'
+    }, 2000)
+
+}
