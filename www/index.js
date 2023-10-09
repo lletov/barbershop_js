868ddf147e9ce8fa125fd3e7af2b21b2369b1546
Dmitry <dmitry2595@gmail.com>
2023-10-09 20:44:03 +0300

    feat: modals

--- www/index.js
+++ www/index.js
@@ -24,7 +24,7 @@ let services = [{
 }]
 
 let servicesBlock = document.getElementById('services')
-let servicesSelect = document.getElementById('services-select')
+let servicesSelect = document.getElementById('services-input')
 for (let i=0; i < services.length; i++) {
     let serviceRow =
     `<div class="service-row">
@@ -36,7 +36,7 @@ for (let i=0; i < services.length; i++) {
     let serviceSelect = 
     `<div class="service-select-row" >
         <input type="checkbox" id="service-select-row-${services[i].id}" onclick="changeValue(${services[i].id})">
-        <label for="">${services[i].name} (${services[i].price} р.)</label>
+        <label for="" value="${services[i].name}">${services[i].name} (${services[i].price} р.)</label>
     </div>`
     servicesSelect.innerHTML += serviceSelect
 
@@ -57,12 +57,12 @@ function backStyle(){
 }
 
 // select barber
-// const barberSelector = document.getElementById('select-barber')
-// barberSelector.value = ''
-// function selectThisBarber(barber) {
-//     barberSelector.value = barber
-//     document.getElementById('visit').scrollIntoView()
-// }
+const barberSelector = document.getElementById('select-barber')
+barberSelector.value = ''
+function selectThisBarber(barber) {
+    barberSelector.value = barber
+    document.getElementById('visit').scrollIntoView()
+}
 
 
 // tab 
@@ -108,18 +108,19 @@ function getDiscount() {
 }
 
 // visit
-
+let selectedServices = []
 function changeValue(id) {
     total = document.getElementById('total').textContent
     totalElement = document.getElementById('total')
     totalElement.innerHTML = '0'
-    let res = Number(total) + 15
     if (document.getElementById(`service-select-row-${id}`).checked == true) {
         let res = Number(total) + services[id].price
         totalElement.innerHTML = `${res}`
+        selectedServices.push(services[id].name)
     } else {
         let res = Number(total) - services[id].price
         totalElement.innerHTML = `${res}`
+        selectedServices = selectedServices.filter(item => item !== services[id].name)
     }
 }
 
@@ -144,3 +145,76 @@ for (let i = 0; i < accordion.length; i++) {
           }
     })
 }
+
+// modal
+const modal = document.querySelector('.modal')
+const modalContent = document.querySelector('modal-content')
+const openModalBtn = document.getElementById('create-visit')
+const closeModalBtn = document.getElementById('close-modal')
+
+function openModal(){
+    if (document.getElementById('name-input').value != '' 
+    && document.getElementById('date-input').value != ''
+    && selectedServices.length != 0
+    && document.getElementById('phone-input').value != ''
+    && document.getElementById('phone-input').value.length == 2+1+3+1+3+3+3
+    && document.getElementById('select-barber').options[document.getElementById('select-barber').selectedIndex].text != null
+    && document.getElementById('personal-data-input').checked == true
+    ) {
+        modal.style.display = 'flex'
+        document.querySelector('body').style.overflowY = 'hidden'
+        fillModal()
+    } else {
+        document.getElementById('not-all-fields').style.display = 'block'
+        setTimeout(() => {
+            document.getElementById('not-all-fields').style.display = 'none'   
+        }, 5000)
+    }
+}
+openModalBtn.onclick = openModal
+
+function closeModal(){
+    modal.style.display = 'none'
+    document.querySelector('body').style.overflowY = 'auto'
+}
+closeModalBtn.onclick = closeModal
+
+window.onclick = function (event) {
+    if (event. target == modal) {
+        closeModal()
+}}
+
+window. addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        closeModal()
+    }
+})
+
+// insert info in modal-content
+function fillModal(){
+    let modalText = document.querySelector('.modal-text')
+    modalText.innerHTML = `<div class="modal-text">
+    <h2>${document.getElementById('name-input').value}, <br> проверьте данные:</h2>
+    <h3>Мастер:</h3>
+    <p>${document.getElementById('select-barber').options[document.getElementById('select-barber').selectedIndex].text}</p>
+    <h3>Дата:</h3>
+    <p>${document.getElementById('date-input').value}</p>
+    <h3>Услуги:</h3>
+    <p>${selectedServices}</p>
+    <h3>Итого:</h3>
+    <p>${document.getElementById('total').textContent}</p>
+    <h3>Ваш номер:</h3>
+    <p>${document.getElementById('phone-input').value}</p>
+    <button id='confirm-visit' onclick = 'openFinalModal()'>Подтвердить и записаться</button>
+    </div>`
+    
+}
+
+function openFinalModal() {
+    closeModal()
+    document.querySelector('.modal-final').style.display = 'block'
+    setTimeout(() => {
+        document.querySelector('.modal-final').style.display = 'none'
+    }, 5000)
+}
+    
