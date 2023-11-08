14f654af9748bf95edfaf85d28075caf05efd186
Dmitry <dmitry2595@gmail.com>
2023-11-08 21:48:29 +0300

    feat: tab data prepare

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 console.log('index.js connected')
+
+// render services from list
+let services = [{
+    id: 0,
+    name: 'Мужская стрижка',
+    price: 1800,
+    time: 45,
+    category: 'hair'
+}, {
+    id: 0,
+    name: 'Стрижка FADE',
+    price: 2200,
+    time: 45,
+    category: 'hair'
+}, {
+    name: 'Стрижка машинкой',
+    price: 950,
+    time: 25,
+    category: 'hair'
+}, {
+    id: 0,
+    name: 'Моделирование бороды',
+    price: 1400,
+    time: 35,
+    category: 'beard'
+}, {
+    id: 0,
+    name: 'Стрижка усов и бороды',
+    price: 900,
+    time: 30,
+    category: 'beard'
+}, {
+    id: 0,
+    name: 'Опасное бритье',
+    price: 1700,
+    time: 40,
+    category: 'beard'
+}, {
+    id: 0,
+    name: 'Мужская стрижка + стрижка усов и бороды',
+    price: 2800,
+    time: 85,
+    category: 'complex'
+}, {
+    id: 0,
+    name: 'Стрижка машинкой и опасное бритье',
+    price: 2100,
+    time: 65,
+    category: 'complex'
+}]
